2bb87037263d4cc3f6b7f66833cfc4ed288abc8a
Buddhima65 <IT23809406@my.sliit.lk>
2026-08-10 15:22:20 +0530

    add Scroll to top page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,11 +6,13 @@ import AboutUs from './pages/AboutUs.jsx'
 import Services from './pages/Services.jsx'
 import ContactUs from './pages/ContactUs.jsx'
 import Portfolio from './pages/Portfolio.jsx'  // ← Add this
+import ScrollToTop from './components/ScrollToTop.jsx'  // ← Add this
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about-us" element={<AboutUs />} />
