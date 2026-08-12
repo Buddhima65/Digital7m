666225f775dc7836049fb32a71f8c9ae2cd6e9fb
Buddhima65 <IT23809406@my.sliit.lk>
2026-08-12 13:42:02 +0530

    change some codes for fixing SEO issue

--- client/src/data/teamData.js
+++ client/src/data/teamData.js
@@ -8,7 +8,7 @@ const teamData = [
     email: 'Mihindumf@gmail.com',
     phone: '+94 77 473 4086',
     bio: '20 years as an investigative journalist targeting multiple outreach channels.15 years as an expert web and internet media designer.10 years as one of the most sort after social media influencers in Sri Lanka. 7 years as a digital media strategist working in tandem with top tier marketing agencies.3 years as a senior expert in digital media at the highest level of government ',
-    image: '/src/assets/images/team/mihindu.jpg',
+    image: '/assets/images/team/mihindu.jpg',
     social: {
       linkedin: 'https://linkedin.com/in/mihindu7/',
       twitter: 'https://twitter.com/immihindu',
@@ -24,7 +24,7 @@ const teamData = [
     email: 'chamodnirmal00@gmail.com',
     phone: '+94 76 112 2324',
     bio: 'A creative professional specializing in web development, graphic design, branding, and digital content creation, with expert knowledge of Adobe Photoshop and Illustrator. Known for his enthusiasm, teamwork, dedication, and strategic thinking, he combines technical expertise with creativity to deliver impactful visual and digital solutions.',
-    image: '/src/assets/images/team/chamod.jpg',
+    image: '/assets/images/team/chamod.jpg',
     social: {
       linkedin: 'https://linkedin.com/in/sankalpa',
      // github: 'https://github.com/sankalpa',
@@ -41,7 +41,7 @@ const teamData = [
     email: 'kfernando467@gmail.com',
     phone: '+94 72 925 9122',
     bio: 'A skilled News Writer and Video Editor specializing in Adobe Premiere Pro, After Effects, and CapCut. He combines compelling storytelling with professional video production while staying up to date with global trends. Known for his motivation, teamwork, dedication, and creative approach, he transforms news into engaging visual experiences.',
-    image: 'src/assets/images/team/kasun.jpg',
+    image: '/assets/images/team/kasun.jpg',
     social: {
       //linkedin: 'https://linkedin.com/in/kamal',
       //dribbble: 'https://dribbble.com/kamal',
@@ -58,7 +58,7 @@ const teamData = [
     email: 'wimalarathnelakmali55@gmail.com',
     phone: '+94 71 487 5343',
     bio: 'Digital marketing expert with expertise in SEO, SEM, and social media marketing. Helps brands reach their target audienceA creative digital media professional specializing in news writing, content creation, and social media management. Skilled in news reporting, feature and editorial writing, press releases, article writing, social media content, copywriting, website content, caption writing, and marketing content. Experienced in social media strategy, content planning and scheduling, Facebook management, community engagement, and performance optimization. Known for creative storytelling, strategic thinking, strong communication, reliability, adaptability, collaboration, and attention to detail, delivering engaging content that strengthens brand presence and audience connections.',
-    image: '/src/assets/images/team/lakmali.jpg',
+    image: '/assets/images/team/lakmali.jpg',
     social: {
       //linkedin: 'https://linkedin.com/in/nimal',
       //twitter: 'https://twitter.com/nimal',
@@ -74,7 +74,7 @@ const teamData = [
     email: 'buddhimasankalapa@gmail.com',
     phone: '+94 71 383 3657',
     bio: 'A dedicated Full Stack Web Developer with expertise in frontend and backend development, building responsive websites and web applications using React, Node.js, Express, MongoDB, and WordPress. Passionate about clean code, performance, and user experience.',
-    image: '/src/assets/images/team/buddhima.jpg',
+    image: '/assets/images/team/buddhima.jpg',
     social: {
       linkedin: 'https://linkedin.com/in/buddhima-sankalpa-a0a212385/',
       //twitter: 'https://twitter.com/chathuri'
